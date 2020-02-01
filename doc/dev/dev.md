f0368c8565b7d415f833f55f432ae214ed265bdb
baolilei <baolilei@100tal.com>
2020-02-01 09:40:36 +0800

    修复已知bug

--- doc/dev/dev.md
+++ doc/dev/dev.md
@@ -2,4 +2,12 @@
 2、修复依赖安装问题。done
 3、测试编写样式 done
 4、安装问题 `npm install pv-cli --unsafe-perm=true --allow-root -g` done
-5、安装后vue-loader不生效 不能在node_modules下
+5、安装后vue-loader不生效 不能在node_modules或者，nvm下  移到～/pv-cli/template done
+
+6、bug 
+
+This dependency was not found:
+
+* vue in ../index.vue 
+
+done 在pv-cli加入vue依赖,在.vue文件下必须vue依赖,postcss.config.js文件
--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -23,12 +23,12 @@ const devConfig = merge(baseWebpackConfig, {
   module: {
     rules: [{
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
     ]
   },
